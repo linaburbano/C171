2622bd67a582c0d4476d9ffd50077b1f6e893928
Lina Burbano <lburbanor19@gmail.com>
2023-08-27 12:26:05 -0500

    segundo trabajo en clase B3

---
+++ src/main/java/B3/SegundoTrabajoEnClaseB3.java
@@ -0,0 +1,24 @@
+package B3;
+
+public class SegundoTrabajoEnClaseB3 {
+    public static void main(String[] args) {
+        //encontrar los numeros primos del 1 hasta 1000 utilizando while o do while o for
+
+        int num = 2;
+        while (num <= 1000) {
+            boolean esPrimo = true;
+            int i = 2;
+            while (i <= Math.sqrt(num)) {
+                if (num % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+                i++;
+            }
+            if (esPrimo) {
+                System.out.print(num + " ");
+            }
+            num++;
+        }
+    }
+}
